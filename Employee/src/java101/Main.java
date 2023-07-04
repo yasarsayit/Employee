6dd1d860b18da66d85dac50084b36e93f2ed986f
yasarsayit <90604486+yasarsayit@users.noreply.github.com>
2023-07-04 13:59:36 +0300

    Add files via upload

---
+++ Employee/src/java101/Main.java
@@ -0,0 +1,12 @@
+package java101;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Employee e1 = new Employee("Yasar", 3000, 45, 1985);
+		Employee e2 = new Employee("Basak", 2000, 50, 2000);
+		e1.printInfo();
+		e2.printInfo();
+
+	}
+}
